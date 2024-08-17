f6a4775b7262a913731ab89f1c3e17376218bbf7
Vishnuprasad S <vishnuprasad.s@scrapehero.com>
2024-08-17 23:48:40 +0530

    dev-init

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { SignUpBox, LoginBox, Slider } from "./Auth.tsx"
+
+import "./IntroMessage.scss";
+import "./Auth.scss";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <IntroMessage />
+        <AuthBox />
+      </div>
+    </>
+  );
+}
+
+function IntroMessage() {
+  return (
+    <div className="intro">
+      <div className="intro-signup">
+        <h1 className="heading-primary--main">New User ?</h1>
+        <h1 className="heading-primary--sub">
+          Sign Up and join us by creating an account.
+        </h1>
+      </div>
+      <div className="intro-login">
+        <h1 className="heading-primary--main">Welcome !</h1>
+        <h1 className="heading-primary--sub">
+          Sign In and enjoy.
+        </h1>
+
+      </div>
+    </div>
+  );
+}
+
+function AuthBox() {
+
+  const [showLogin, setShowLogin] = useState(true);
+
+  function handleAuth() {
+    setShowLogin(!showLogin);
+  }
+
+  let classes = "auth";
+  if (showLogin == false) {
+    classes = "auth slide-animation";
+  }
+
+  return (
+    <>
+      <div className={classes}>
+        <SignUpBox showLogin={showLogin} />
+        <Slider showLogin={showLogin} handleAuth={handleAuth} />
+        <LoginBox showLogin={showLogin} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Auth.tsx
@@ -0,0 +1,182 @@
+import {useState, useRef, useLayoutEffect } from 'react';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+export function SignUpBox({ showLogin }: { showLogin: boolean }) {
+  return (
+    <TransitionGroup className="auth-section">
+      {!showLogin && (
+        <CSSTransition
+          in={showLogin}
+          timeout={{ enter: 1000, exit: 1000 }}
+          classNames="auth-form__signup"
+        >
+          <div className="auth-form">
+            <h1 className="heading-primary--main">Create an Account</h1>
+            <div className="icon">
+              <span className="icon__init_text">or join with:</span>
+              <img
+                className="icon__img"
+                src="src/assets/icons8-google-plus.svg"
+                alt="no image"
+              />
+              <img
+                className="icon__img"
+                src="src/assets/icons8-facebook.svg"
+                alt="no image"
+              />
+              <img
+                className="icon__img"
+                src="src/assets/icons8-linked-in.svg"
+                alt="no image"
+              />
+            </div>
+            <form className="auth-form__signup" method="post">
+              <input
+                className="auth-form-field-box"
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Username"
+                required
+              />
+              <label className="auth-form-field-label" htmlFor="username">
+                Username
+              </label>
+              <input
+                className="auth-form-field-box"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+              <label className="auth-form-field-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="auth-form-field-box"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+              <label className="auth-form-field-label" htmlFor="password">
+                Password
+              </label>
+              <button className="btn" type="submit">
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
+  );
+}
+
+export function LoginBox({ showLogin }: { showLogin: boolean }) {
+  return (
+    <TransitionGroup className="auth-section">
+      {showLogin && (
+        <CSSTransition
+          in={showLogin}
+          timeout={{ enter: 1000, exit: 1000 }}
+          classNames="auth-form__login"
+        >
+          <div className="auth-form">
+            <h1 className="heading-primary--main">Sign In</h1>
+            <div className="icon">
+              <span className="icon__init_text">or sign in with:</span>
+              <img
+                className="icon__img"
+                src="src/assets/icons8-google-plus.svg"
+                alt="no image"
+              />
+              <img
+                className="icon__img"
+                src="src/assets/icons8-facebook.svg"
+                alt="no image"
+              />
+              <img
+                className="icon__img"
+                src="src/assets/icons8-linked-in.svg"
+                alt="no image"
+              />
+            </div>
+            <form className="auth-form__login" method="post">
+              <input
+                className="auth-form-field-box"
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Username"
+                required
+              />
+              <label className="auth-form-field-label" htmlFor="username">
+                Username
+              </label>
+              <input
+                className="auth-form-field-box"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+              <label className="auth-form-field-label" htmlFor="password">
+                Password
+              </label>
+              <div className="password-recovery">
+                <a href="">Forgot your password?</a>
+              </div>
+              <button className="btn" type="submit">
+                Sign In
+              </button>
+            </form>
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
+  );
+}
+
+export function Slider({
+  showLogin,
+  handleAuth,
+}: {
+  showLogin: boolean;
+  handleAuth: () => void;
+}) {
+  const [classes, setClasses] = useState("slider");
+  const onceClicked = useRef(false);
+
+  useLayoutEffect(() => {
+    if (!onceClicked.current) {
+      return
+    }
+
+    setClasses("slider slider-stretch");
+    const countdownInterval = setTimeout(() => {
+      setClasses("slider");
+    }, 500);
+
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, [showLogin]);
+
+  let btnToRender = "SIGN IN";
+  if (showLogin) {
+    btnToRender = "SIGN UP";
+  }
+  return (
+    <div className={classes}>
+      <button className="btn btn-intro" onClick={() => {
+        handleAuth(); onceClicked.current = true;
+      }}>
+        {btnToRender}
+      </button>
+    </div>
+  );
+}
